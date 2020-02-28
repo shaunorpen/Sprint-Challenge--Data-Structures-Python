54084ed8da766a763e8e5bac7f0ff5c34d91473e
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-02-28 09:58:14 +0000

    Add stretch names

--- names/names.py
+++ names/names.py
@@ -37,4 +37,30 @@ print (f"runtime: {end_time - start_time} seconds")
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
 
-# Stretch goal answer: duplicates.extend(list(set(names_1).intersection(set(names_2))))
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates = []  # Return the list of duplicates in this data structure
+
+# Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# Solution 1: Fastest with standard data types
+# duplicates.extend(list(set(names_1).intersection(set(names_2))))
+
+# Solution 2: Uses only lists
+duplicates.extend([name for name in names_1 if name in names_2])
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
