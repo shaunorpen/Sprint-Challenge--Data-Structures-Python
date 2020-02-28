06804a9b5c9ef94a7551312a6dab45bde8b0ae11
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-02-28 09:15:48 +0000

    Add MVP for names

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -13,7 +14,19 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-duplicates.extend(list(set(names_1).intersection(set(names_2))))
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+name_tree = BinarySearchTree(names_1[0])
+
+for name in names_1[1:]:
+    name_tree.insert(name)
+
+for name in names_2:
+    if name_tree.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -23,3 +36,5 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# Stretch goal answer: duplicates.extend(list(set(names_1).intersection(set(names_2))))
